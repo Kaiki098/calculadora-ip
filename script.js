7a8d8d20b1d4d7fc3c970a5e94663b0abde3503e
kaiki <kaikialvarengasouza098@gmail.com>
2025-01-06 15:40:56 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const input = document.getElementById("ip-address");
+const button = document.getElementById("button");
+
+button.addEventListener("click", () => {
+  console.log(input.value);
+
+});
