2dd4b6a199ece97a6b3b72d97e7bcdcb63946e14
Kaiki Alvarenga de Souza <127666620+Kaiki098@users.noreply.github.com>
2025-01-21 11:42:03 +0000

    update input placeholder and implement IP to binary conversion function

--- script.js
+++ script.js
@@ -4,4 +4,39 @@ const button = document.getElementById("button");
 button.addEventListener("click", () => {
   console.log(input.value);
 
+  // TODO fazer verificação se é um IP válido
+
+
+  converteIPParaBinario(input.value);
 });
+
+function converteIPParaBinario (ip) {
+  const mascaraEip = ip.split('/');
+  const numeros = mascaraEip[0].split('.').map((numero) => parseInt(numero));
+  
+  // Extração do IP
+  const converteNumeroParaBinario = (numero) => {
+    let pilha = [];
+
+    while (numero != 0) {
+      if (numero % 2 == 0)  {
+        pilha.push(0);
+      } else pilha.push(1);
+
+      numero = Math.floor(numero / 2);
+    }
+
+    return pilha.reverse().join('');
+  }
+
+  const binariosIP = numeros.map((numero) => converteNumeroParaBinario(numero));
+  
+  // Extração da máscara
+  let numeroMascara = parseInt(mascaraEip[1]);
+  let mascara = Array(4).map((l) => {
+    l = new Array(8).fill(1, 0, numeroMascara % 9); // FIXME Não é módulo
+    numeroMascara = numeroMascara - 8;
+  });
+  
+  console.log(mascaraEip, numeros, binariosIP, mascara);
+}
